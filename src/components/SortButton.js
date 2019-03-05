83557d2d3047decc9d05d05e8080ea01d408dd5a
marcusngwj <marcusngwj@msn.com>
2019-03-06 01:53:30 +0800

    Add color to button

--- src/components/SortButton.js
+++ src/components/SortButton.js
@@ -1,3 +1,5 @@
+import './../../assets/styles/sort-button.scss';
+
 export class SortButton {
   constructor(text, onClickCallback, sortFunction) {
     this._onClick = this._onClick.bind(this);
@@ -13,7 +15,10 @@ export class SortButton {
 
   render() {
     const button = document.createElement('button');
+    button.setAttribute('class', 'button-sort');
+
     const text = document.createTextNode(this._text);
+
     button.appendChild(text);
     button.addEventListener('click', this._onClick);
     return button;
