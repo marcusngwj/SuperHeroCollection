f7147a6c22c683fe845efe749a0a7510c4874ed1
marcusngwj <marcusngwj@msn.com>
2019-03-14 14:20:00 +0800

    add react components

---
+++ SuperHeroCollection_Advanced/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import 'Styles/app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>hello</div>
+    );
+  }
+}
+
+export default App;
